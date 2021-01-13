d7c1410c5108e71f4f17e8e6bc101c1e425bd19d
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2021-01-13 14:46:50 -0800

    add styling

--- src/components/Box.js
+++ src/components/Box.js
@@ -4,12 +4,16 @@ const Box = ({ rowIndex, boxIndex, renderBoxText, onChange, player }) => {
   const [clicked, setClicked] = useState(false);
 
   return (
-    <button
-      className="box ui button"
-      onClick={() => onChange(rowIndex, boxIndex, player, clicked, setClicked)}
-    >
-      {renderBoxText}
-    </button>
+    <div className="box">
+      <button
+        className="box ui button"
+        onClick={() =>
+          onChange(rowIndex, boxIndex, player, clicked, setClicked)
+        }
+      >
+        {renderBoxText}
+      </button>
+    </div>
   );
 };
 
